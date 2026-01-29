cb9ce01bcd49b3920f6d40566aa0b3400c7b70dc
slamasamar4 <slamasamar430@gmail.com>
2026-01-29 16:04:45 +0100

    setting auth using better auth docs

---
+++ lib/auth.ts
@@ -0,0 +1,18 @@
+import {betterAuth} from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+//  better auth give you tools to easily implement authentication and authorization in your application.u can easily set up bur own database .it  supports various databases like mongodb ,postgresql ,mysql ,sqlite etc.
+
+const client = new MongoClient(process.env.MONGODB_URI as string);
+const db = client.db();
+         //secret key used to sign and verify tokens.make sure to use a strong and unique secret key in production.
+
+export const auth = betterAuth({
+  
+    database: mongodbAdapter(db, {
+        client,
+    }),
+    emailAndPassword: {
+        enabled: true,
+    },
+    })
