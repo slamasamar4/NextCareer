37651978e70b2c4a97076ec6f7606e8251ea0a59
slamasamar4 <slamasamar430@gmail.com>
2026-02-03 21:51:29 +0100

    Session check

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,31 +1,46 @@
-import {betterAuth} from "better-auth";
+import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-//  better auth give you tools to easily implement authentication and authorization in your application.u can easily set up bur own database .it  supports various databases like mongodb ,postgresql ,mysql ,sqlite etc.
+import { initializeUserBoard } from "../init-user-board";
 
 const client = new MongoClient(process.env.MONGODB_URI as string, {
-    tls: true,
-    tlsAllowInvalidCertificates: true,
-    tlsAllowInvalidHostnames: true,
-    minPoolSize: 0,
-    maxPoolSize: 10,
-    serverSelectionTimeoutMS: 30000,
-    socketTimeoutMS: 30000,
+  tls: true,
+  tlsAllowInvalidCertificates: true,
+  tlsAllowInvalidHostnames: true,
+  minPoolSize: 0,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 30000,
 });
 const db = client.db("nextcareer");
-         //secret key used to sign and verify tokens.make sure to use a strong and unique secret key in production.
 
 export const auth = betterAuth({
-  
-    database: mongodbAdapter(db, {
-        client,
-    }),
-    emailAndPassword: {
-        enabled: true,
+  database: mongodbAdapter(db, {
+    client,
+  }),
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60,
     },
-    });
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          if (user.id) {
+            await initializeUserBoard(user.id);
+          }
+        },
+      },
+    },
+  },
+});
 
 export async function getSession() {
   const result = await auth.api.getSession({
@@ -34,6 +49,7 @@ export async function getSession() {
 
   return result;
 }
+
 export async function signOut() {
   const result = await auth.api.signOut({
     headers: await headers(),
